82bb38ef7e32b9ee6e4e3ff3c42fbb7792c51a7b
momo <normalguyinvancouver@gmail.com>
2018-04-29 15:57:04 -0700

    changed the terms of use to be more visually friendly and took away the annoying <br> issue

--- Sharefood/post.php
+++ Sharefood/post.php
@@ -9,21 +9,25 @@ require_once('view/top.php');
   <p><input type="text" name="title" placeholder="Title"></p>
   <p><img src="img/photo.png" alt="photo" id="photoupload"> <input type="file" name="image" accept="image/gif, image/jpeg, image/png, image/jpg" value="image"></p>
   <p><textarea name="description" rows="4" cols="20" placeholder="Description"></textarea></p>
+
   <div>
-    <input type="checkbox" class="read-more-state" id="post-1" />
+    <input type="checkbox" id="toggle"/>
+    <label for="toggle">
+        <span class='expand'>
+            <span class="changeArrow arrow-up">-</span>
+            <span class="changeArrow arrow-dn">+</span>
+            Terms of Use
+        </span>
+    </label>
+    <div class="fieldsetContainer">
+        <fieldset>
+           By uploading to this site, you, the user agree that the food item is not expired, nor has it been opened.
+           This site was made under the pretense that a faithful and caring community (BCIT) exists.
+           <br>
+           Please contact <strong>dewkim7@gmail.com</strong> for any administrative issues.
+           </fieldset>
+    </div>
 
-    <p id="p123" class="read-more-wrap" style="font-family:Trebuchet MS"> Terms of Use
-      <span class="read-more-target" > <br>
-           By uploading to this site, you, the user agree
-           that the food item is not expired, nor has it been opened. This site was made
-           under the pretense that a faithful and caring community (BCIT) exists.
-           Please contact lee@seul.ca for any administrative issues.
-      </span>
-    </p>
-
-
-    <label for="post-1" class="read-more-trigger"></label>
-  </div>
   <p><input type="submit" name="submit" value="Upload" class="button"></p>
 </form>
 

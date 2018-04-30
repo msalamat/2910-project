0b611f486d13ba568b61393c60f10a71a4dcfe7d
Leeseul Kim <dewkim7@gmail.com>
2018-04-29 18:12:15 -0700

    changed some css

--- Sharefood/post.php
+++ Sharefood/post.php
@@ -3,41 +3,36 @@ require_once('view/top.php');
  ?>
 
 <div class = "list_item">
-<form action="create_process.php?" name ="posting" onsubmit="return check_input()" method="post" enctype="multipart/form-data">
-  <p><input type="password" name="password" placeholder="Password"><br><span class = "desc">* Required to edit/delete the post later</span></p>
-  <p><input type="email" name="email" placeholder="Email address"><br><span class = "desc">* Used to receive a request from other users</span></p>
-  <p><input type="text" name="title" placeholder="Title"></p>
-  <p><img src="img/photo.png" alt="photo" id="photoupload"> <input type="file" name="image" accept="image/gif, image/jpeg, image/png, image/jpg" value="image"></p>
-  <p><textarea name="description" rows="4" cols="20" placeholder="Description"></textarea></p>
+  <p class="list_title">Share your food</p>
+  <form action="create_process.php?" name ="posting" onsubmit="return check_input()" method="post" enctype="multipart/form-data">
+    <p><input type="password" name="password" placeholder="Password"><br><span class = "desc">* Required to edit/delete the post later</span></p>
+    <p><input type="email" name="email" placeholder="Email address"><br><span class = "desc">* Used to receive a request from other users</span></p>
+    <p><input type="text" name="title" placeholder="Title"></p>
+    <p><img src="img/photo.png" alt="photo" id="photoupload"> <input type="file" name="image" accept="image/gif, image/jpeg, image/png, image/jpg" value="image"></p>
+    <p><textarea name="description" rows="4" cols="20" placeholder="Description"></textarea></p>
 
-  <div>
-    <input type="checkbox" id="toggle"/>
-    <label for="toggle">
-        <span class='expand'>
-            <span class="changeArrow arrow-up">-</span>
-            <span class="changeArrow arrow-dn">+</span>
-            Terms of Use
-        </span>
-    </label>
-    <div class="fieldsetContainer">
-        <fieldset>
-           By uploading to this site, you, the user agree that the food item is not expired, nor has it been opened.
-           This site was made under the pretense that a faithful and caring community (BCIT) exists.
-           <br>
-           Please contact <strong>dewkim7@gmail.com</strong> for any administrative issues.
-           </fieldset>
+    <div class="toggleDiv">
+      <input type="checkbox" id="toggle"/>
+      <label for="toggle">
+          <span class='expand'>
+              Terms of Use
+              <span class="changeArrow arrow-up"><img src="img/arrow-up.png" alt="up"></span>
+              <span class="changeArrow arrow-dn"><img src="img/arrow-down.png" alt="down"></span>
+          </span>
+      </label>
+      <div class="fieldsetContainer">
+         <p>By uploading to this site, you, the user agree that the food item is not expired, nor has it been opened.
+         This site was made under the pretense that a faithful and caring community (BCIT) exists.</p>
+      </div>
+      <p>I have read and agree to the Terms of Use.</p>
+      <input type="checkbox" name="checkbox" value="check">
+      <p><input type="submit" name="submit" value="Upload" class="button" onclick="if(!this.form.checkbox.checked){alert('You must agree to the terms first.');return false}"></p>
     </div>
 
-    <p style="margin-bottom: 0px;">I have read and agree to the Terms of Use.</p>
-    <input type="checkbox" name="checkbox" value="check">
-    <p><input type="submit" name="submit" value="Upload" class="button" onclick="if(!this.form.checkbox.checked){alert('You must agree to the terms first.');return false}"></p>
-
-</form>
-
-
-
+  </form>
 
 </div>
+
 <br>
 
 <?php

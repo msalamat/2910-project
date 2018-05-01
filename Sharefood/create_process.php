b695a9838bd5b445af50e4cd2cf0014bad127205
Eric Zhang <ericweikaizhang@gmail.com>
2018-05-01 12:14:34 -0700

    updated version

--- Sharefood/create_process.php
+++ Sharefood/create_process.php
@@ -4,44 +4,8 @@
     require_once('config/config.php');
     $conn = db_init($config["host"], $config["dbuser"], $config["dbpw"], $config["dbname"]);
 
-<<<<<<< HEAD
-    // Upload and Resize the photo
-    $post_photo = basename($_FILES['image']['name']);
-    $post_tmp = $_FILES['image']['tmp_name'];
-    $dir = "uploads/";
-    $len = strlen($post_photo);
-    $path = $dir . date('y-m-d-a-h-i-s') . ($len > 20 ? substr($post_photo, $len-10) : $post_photo); //path to be uploaded
-    //if file name is over 20, make it short
-
-    $ext = strtolower(pathinfo($post_photo, PATHINFO_EXTENSION)); //getting image extension
-    if($ext == 'jpg' || $ext == 'jpeg'){
-      $src = imagecreatefromjpeg($post_tmp);
-    } else if($ext == 'png'){
-      $src = imagecreatefrompng($post_tmp);
-    } else if($ext == 'gif'){
-      $src = imagecreatefromgif($post_tmp);
-    }
-
-    list($width, $height) = getimagesize($post_tmp); // fetching original image width and height
-    $newwidth = 350;
-    $newheight = $height / $width * $newwidth;  // calculating proportionate height
-    $tmp_min = imagecreatetruecolor($newwidth, $newheight); // creating frame for compress image
-
-    // make background color white
-    $white = imagecolorallocate($tmp_min, 255, 255, 255);
-    imagefill($tmp_min, 0, 0, $white);
-
-    imagecopyresampled($tmp_min, $src, 0,0,0,0, $newwidth, $newheight, $width, $height);  //compressing image
-
-    // extracting only strings to prevent injection attack
-
-    $filtered = array(
-      'password' => mysqli_real_escape_string($conn, $_POST['password']),
-      'email' => mysqli_real_escape_string($conn, $_POST['email']),
-      'title' => mysqli_real_escape_string($conn, $_POST['title']),
-      'description' => mysqli_real_escape_string($conn, $_POST['description']),
-    );
-=======
+  
+   
 // extracting only strings to prevent injection attack
 
 $filtered = array(
@@ -52,16 +16,8 @@ $filtered = array(
   'location' => $_POST['location'],
   'image' => mysqli_real_escape_string($conn,$_POST['image']),
 );
->>>>>>> 324ff3d4efa1664f1a8b43fc5267d3f5abde4da9
 
 
-<<<<<<< HEAD
-    //connect to swear.php
-    // require("swear.php");
-    //
-    // $filtered['title']=str_replace($find, $replace, $filtered['title']);
-    // $filtered['description']=str_replace($find, $replace, $filtered['description']);
-=======
 $sql = "
     INSERT INTO list
     (password, email, title, status, image, description, created, location)
@@ -77,28 +33,7 @@ $sql = "
       )
       ";
 
-$result = mysqli_query($conn, $sql);
-if($result === false){
-  echo "Sorry! there's a problem in the server<br>";
-  echo "<a href='post.php'>Go back</a><br>";
->>>>>>> 324ff3d4efa1664f1a8b43fc5267d3f5abde4da9
-
-
-
-    // Insert data into table
-    $sql = "
-        INSERT INTO list
-        (password, email, title, status, image, description, created)
-        VALUES(
-          '{$filtered['password']}',
-          '{$filtered['email']}',
-          '{$filtered['title']}',
-          'Available',
-          '{$path}',
-          '{$filtered['description']}',
-          NOW()
-          )
-          ";
+    
     $result = mysqli_query($conn, $sql);
     if($result === false){
       echo "Sorry! there's a problem in the server<br>";

7bb7d522234aae8ffd79e9a91c1364dbaa8e32f6
Amanda Golubics <amanda.golubics@gmail.com>
2018-05-01 11:37:53 -0700

    Fix location

--- Sharefood/index.php
+++ Sharefood/index.php
@@ -5,6 +5,7 @@ require_once('config/config.php');
 $conn = db_init($config["host"], $config["dbuser"], $config["dbpw"], $config["dbname"]);
  ?>
 
+
 <div class="banner">
   <p id="textAni"></p>
   <form id="search-container" action="index.php" method="GET" name="searching">

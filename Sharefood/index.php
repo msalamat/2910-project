87491afa3aeb735a7491396d233ffb1c5abf8f98
Leeseul Kim <dewkim7@gmail.com>
2018-05-01 22:56:03 -0700

    added loading feature

--- Sharefood/index.php
+++ Sharefood/index.php
@@ -7,10 +7,10 @@ $conn = db_init($config["host"], $config["dbuser"], $config["dbpw"], $config["db
 
 <div class="banner">
   <p id="textAni"></p>
-  <form id="search-container" action="index.php" method="GET" name="searching">
+  <form id="search-container" action="index2.php" method="GET" name="searching">
     <i class="fas fa-search"></i>
     <input id="searchbox" type="text" name="search" placeholder="Search..." >
-    <button id="searchbtn" type="submit" name="submit">Search</button>
+    <button id="searchbtn" type="submit">Search</button>
   </form>
 </div>
 
@@ -19,37 +19,92 @@ $conn = db_init($config["host"], $config["dbuser"], $config["dbpw"], $config["db
 
   if(count($_GET) == 0) {
     $sql = "SELECT * FROM list ORDER BY id DESC";
+    $sql_last = "SELECT id FROM list LIMIT 1"; // the number of rows
   } else {
     $q = $_GET['search'];
     $sql = "SELECT * FROM list WHERE title LIKE '%$q%' OR description LIKE '%$q%' ORDER BY id DESC";
+    $sql_last = "SELECT id FROM list WHERE title LIKE '%$q%' OR description LIKE '%$q%' LIMIT 1";
   }
-    
+  
+  // returns the last item's id
+  $result_last = mysqli_query($conn, $sql_last);
+  $last_array = mysqli_fetch_array($result_last);
+  $last = is_null($last_array[0]) ? 0 : $last_array[0];
+
   $result = mysqli_query($conn, $sql);
+  
+  $lastid = 0;
   if ($result != null){
 
-    while($row = mysqli_fetch_array($result)){
+    $i = 0;
+    while ($i < 3) {
+      if($row = mysqli_fetch_array($result)) {
+        //prevent cross scripting attack
+        $escaped = array(
+          'title' => htmlspecialchars($row['title']),
+          'image' => htmlspecialchars($row['image']),
+          'status' => htmlspecialchars($row['status']),
+        );
 
-      //prevent cross scripting attack
-      $escaped = array(
-        'title' => htmlspecialchars($row['title']),
-        'image' => htmlspecialchars($row['image']),
-        'status' => htmlspecialchars($row['status']),
-      );
+        // substring y-m-d
+        $created = substr($row['created'], 0, 10);
 
-      // substring y-m-d
-      $created = substr($row['created'], 0, 10);
-
-      // listing each poast
-      echo "<a href=\"detail.php?id={$row['id']}\"><div class='list_item'><p class='list_title'>{$escaped['title']}</p>";
-      echo "<img src=\"{$escaped['image']}\" class='uploadedImg'>
-      <p>{$escaped['status']}<br>{$created}</p>
-      </div></a>";
-    };
+        // listing each poast
+        echo "<a href=\"detail.php?id={$row['id']}\"><div class='list_item'><p class='list_title'>{$escaped['title']}</p>";
+        echo "<img src=\"{$escaped['image']}\" class='uploadedImg'>
+        <p>{$escaped['status']}<br>{$created}</p>
+        </div></a>";
+        $lastid = $row['id'];
+      }
+      $i++;
+    }
 
   }
+
 ?>
+<br>
+<div id="loaded"></div>
+<button id="loadButton">More results &nbsp; <img src="img/arrow-down.png"></button>
+
 <button id="plusButton"><a href="post.php"><img src="img/plus.png" alt="post"></a>
 </button>
+
+<script>
+  var lastId = <?=$lastid?>;
+  var result = <?=$last?>;
+
+  (function(){
+      if(lastId == result){
+            $("#loadButton").css("display", "none");
+          }
+    })();
+
+  var search = "<?php
+      if(isset($_GET['search'])){
+        echo $_GET['search'];
+      } else {
+      }
+      ?>";
+
+    $("#loadButton").click(function(event){
+      $.ajax({
+        url: "load_process.php",
+        type: "POST",
+        data: {id: lastId, q: search},
+        success: function(data){
+          $("#loaded").append(data);
+          lastId = newid;
+          if(lastId == result){
+            $("#loadButton").css("display", "none");
+          }
+        }
+        
+      });
+    });
+
+    
+
+</script>
 
 <script src="js/typewriter.js"></script>
 <?php
---
+++ Sharefood/load_process.php
@@ -0,0 +1,46 @@
+<?php
+require_once('lib/connect.php');
+require_once('config/config.php');
+$conn = db_init($config["host"], $config["dbuser"], $config["dbpw"], $config["dbname"]);
+
+$id = $_POST['id'];
+$q = $_POST['q'];
+
+echo "<script>var newid = {$_POST['id']};</script>"; //by default
+
+if($q == "") {
+    $sql = "SELECT * FROM list WHERE id < {$id} ORDER BY id DESC";
+  } else {
+    $sql = "SELECT * FROM list WHERE (title LIKE '%$q%' OR description LIKE '%$q%') AND id < {$id} ORDER BY id DESC";
+  }
+
+$result = mysqli_query($conn, $sql);
+  if ($result != null){
+
+    $i = 0;
+    while ($i < 3) {
+      if($row = mysqli_fetch_array($result)) {
+        //prevent cross scripting attack
+        $escaped = array(
+          'title' => htmlspecialchars($row['title']),
+          'image' => htmlspecialchars($row['image']),
+          'status' => htmlspecialchars($row['status']),
+        );
+
+        // substring y-m-d
+        $created = substr($row['created'], 0, 10);
+
+        // listing each post
+        echo "<a href=\"detail.php?id={$row['id']}\"><div class='list_item'><p class='list_title'>{$escaped['title']}</p>";
+        echo "<img src=\"{$escaped['image']}\" class='uploadedImg'>
+        <p>{$escaped['status']}<br>{$created}</p>
+        </div></a>";
+        echo "<script>newid = {$row['id']};</script>";
+      }
+      $i++;
+      
+    }
+    
+  } 
+
+ ?>

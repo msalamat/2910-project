376b0bb8948a4fdadeeac3693d70d441d010641a
momo <normalguyinvancouver@gmail.com>
2018-05-02 18:55:54 -0700

    made some changes

---
+++ Sharefood/info.php
@@ -0,0 +1,94 @@
+<?php
+require_once('view/top.php');
+ ?>
+
+
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style/info.css">
+  </head>
+  <body>
+    <div class="container">
+    <ol class="step-list">
+        <li class="step-list__item">
+            <div class="step-list__item__inner">
+                <div class="content">
+                    <div class="body">
+                        <h2>60% of the Third World Have Poor Drinking Water</h2>
+                        <p>Are you going to let this continue?</p>
+                    </div>
+
+                    <div class="icon">
+                        <img src="" alt="one pic" />
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="step-list__item">
+            <div class="step-list__item__inner">
+                <div class="content">
+                    <div class="body">
+                        <h2>Because you shouldn't</h2>
+                        <p>(please don't)</p>
+                    </div>
+
+                    <div class="icon">
+                        <img src="" alt="other svg" />
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="step-list__item">
+            <div class="step-list__item__inner">
+                <div class="content">
+                    <div class="body">
+                        <h2>Think of the kids</h2>
+                        <p>(the fat ones too)</p>
+                    </div>
+
+                    <div class="icon">
+                        <img src="" alt="another svg" />
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="step-list__item">
+            <div class="step-list__item__inner">
+                <div class="content">
+                    <div class="body">
+                        <h2>Think of the ice-cream</h2>
+                        <p>(mango ice-cream!)</p>
+                    </div>
+
+                    <div class="icon">
+                        <img src="" alt="another svg" />
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="step-list__item">
+            <div class="step-list__item__inner">
+                <div class="content">
+                    <div class="body">
+                        <h2>Oh oh oh </h2>
+                        <p>Ha ha ha</p>
+                    </div>
+
+                    <div class="icon">
+                        <img src="" alt="another svg" />
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ol>
+</div>
+  </body>
+</html>
+
+<?php
+require_once('view/footer.php');
+ ?>
--- Sharefood/view/top.php
+++ Sharefood/view/top.php
@@ -6,11 +6,14 @@
     <title>Share Food</title>
     <link rel="icon" href="img/logo.png">
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/info.css">
     <!--  reference link to font awesome  -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/dropdown.js"></script>
-    
+    <script src="js/prog.js"></script>
+
+
   </head>
   <body>
     <header>
@@ -18,7 +21,7 @@
       <nav>
         <div class="textmenu">
           <span class="menu"><a href="post.php">SHARE</a></span>
-          <span class="menu"><a href="">FOOD FACTS</a></span>
+          <span class="menu"><a href="info.php">FOOD FACTS</a></span>
           <span class="menu"><a href="">HOW TO</a></span>
           <span class="menu"><a href="">ABOUT</a></span>
         </div>
@@ -27,7 +30,7 @@
           <button id ="myBtn" onclick="popMenu()" class="dropbtn"></button>
           <div class="dropdown-content" id="myDropdown">
             <a href="post.php">SHARE</a>
-            <a href="">FOOD FACTS</a>
+            <a href="info.php">FOOD FACTS</a>
             <a href="">HOW TO</a>
             <a href="">ABOUT</a>
           </div>

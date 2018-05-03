6bff741930a12f328877becdb8347e72fac67774
Leeseul Kim <dewkim7@gmail.com>
2018-05-03 14:55:13 -0700

    css changed

--- Sharefood/js/dropdown.js
+++ Sharefood/js/dropdown.js
@@ -3,11 +3,11 @@ function menu_function(){
     if (temp.className === "topMenu"){
       temp.className += " responsive";
       document.getElementById("nav").style.height = "210px";
-      document.getElementById("banner").style.marginTop = "210px";
+      document.getElementById("article").style.marginTop = "210px";
     } else {
       temp.className = "topMenu";
-      document.getElementById("nav").style.height = "72px";
-      document.getElementById("banner").style.marginTop = "0px";
+      document.getElementById("nav").style.height = "80px";
+      document.getElementById("article").style.marginTop = "80px";
     }
 }
 

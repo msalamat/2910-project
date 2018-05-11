82766afe8c281eeef5deb477b165f38a8014df38
Leeseul Kim <dewkim7@gmail.com>
2018-05-10 21:29:10 -0700

    photo upload

--- Sharefood/js/fileupload.js
+++ Sharefood/js/fileupload.js
@@ -18,27 +18,32 @@ $('#image').change(function(e) {
       processData: false,
     
       xhr: function() {
-            var myXhr = $.ajaxSettings.xhr();
-            if (myXhr.upload) {
-                // For handling the progress of the upload
-                myXhr.upload.addEventListener('progress', function(e) {
-                    if (e.lengthComputable) {
-                        $('progress').attr({
-                            value: e.loaded,
-                            max: e.total,
-                        });
-                    }
-                } , false);
-            }
-            return myXhr;
-        },
+        var myXhr = $.ajaxSettings.xhr();
+        if (myXhr.upload) {
+            // For handling the progress of the upload
+            myXhr.upload.addEventListener('progress', function(e) {
+                if (e.lengthComputable) {
+                    $('progress').attr({
+                        value: e.loaded * 0.9,
+                        max: e.total
+                    });
+                    
+                }
+            } , false);
+        }
+        return myXhr;
+      },
+
       success: function(response){
         $("#tempdiv").css("display", "none");
         $('#img').attr("src", response);
-        $('#imgpath').attr("value", response);
         $('#img').attr("height", "100px");
+        $('progress').attr({
+          value: 1,
+          max: 1
+        });
       }
-  
+
     });
   
     

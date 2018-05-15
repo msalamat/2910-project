1b9977a472835ccf3c3831504ee4fad241122045
Leeseul Kim <dewkim7@gmail.com>
2018-05-15 14:23:56 -0700

    game image

--- Sharefood/config/config.php
+++ Sharefood/config/config.php
@@ -2,11 +2,7 @@
 $config = array(
   "host" => "localhost",
   "dbuser" => "root",
-<<<<<<< HEAD
   "dbpw" => "121212",
-=======
-  "dbpw" => "",
->>>>>>> 416338fcab4b81b83f5427e2d6a5d26bc76b62ec
   "dbname" => "sharefood"
 );
 

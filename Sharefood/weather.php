a18f798c2248182f3ca5c02f16515dbca5b4c491
momo <normalguyinvancouver@gmail.com>
2018-05-16 09:54:47 -0700

    temp weather module

---
+++ Sharefood/weather.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head><title>Weather Report</title>
+
+</head>
+
+<body>
+
+
+	<center><br><br>
+		<form method="GET" action="get.php">
+		<h1>Type City and Country</h1>
+		<br><p>For Example vancouver,bc</p>
+			<input type="text" name="q" required>
+			<input type="submit" name="submit">
+		</form>
+	</center>
+</body>
+</html>

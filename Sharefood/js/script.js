89cc4b6488a9843869a3da8361fd71b4bf2336bf
Leeseul Kim <dewkim7@gmail.com>
2018-05-24 14:29:22 -0700

    script

--- Sharefood/js/script.js
+++ Sharefood/js/script.js
@@ -106,21 +106,18 @@ function check_input(){
   return true;
 }
 
-// uploading msg
-// $("#confirmBtn").click(function(e){
-//   e.preventDefault();
-//   swal({
-//     icon: "success",
-//     title: "You're awesome!",
-//     text: "we're making your post now!",
-//     timer: 20000,
-//     button: false,
-//     onOpen: () => {
-//       swal.showLoading()
-//       }
-//   });
-//   $("#confirmForm").submit();
-// });
+//uploading msg
+$("#confirmBtn").click(function(e){
+  e.preventDefault();
+  swal({
+    icon: "success",
+    title: "You're awesome!",
+    text: "we're making your post now!",
+    timer: 20000,
+    button: false,
+  });
+  $("#confirmForm").submit();
+});
 
 //check input for request page
 function checkRequest(){

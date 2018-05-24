bb8f9309167582822b5a92aaee70f933e4c89f17
Leeseul Kim <dewkim7@gmail.com>
2018-05-24 15:28:40 -0700

    some bugs

--- Sharefood/contact.php
+++ Sharefood/contact.php
@@ -9,7 +9,7 @@ require_once('view/top.php');
     <p class="list_title">Get in Touch</p>
     <p>Send us your questions and we will get back to you!</p>
 
-    <p><input class="textinput" type="text" name="name" id="contact-name" pattern="[A-Za-z]{2,30}" placeholder="Name" required="required">
+    <p><input class="textinput" type="text" name="name" id="contact-name" placeholder="Name" required="required">
     <span class="highlight"></span>
     <span class="bar"></span>
     </p>
